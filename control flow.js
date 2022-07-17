2f329294c7cc087508717f12c8d56accc14518d6
vicowu21 <85970139+vicowu21@users.noreply.github.com>
2022-07-17 22:36:58 +0000

    Create control flow.js

---
+++ control flow.js
@@ -0,0 +1,52 @@
+/*{
+    var age = prompt();
+    if(age <= 13 || age >=65){
+        console.log("Welcome")
+    }
+
+}
+*/
+
+/*{
+    let name = prompt();
+
+    switch(name){
+        case "Vicky":
+            console.log("Welcome");
+            break;
+        case "Justine":
+            console.log("Hey baby")
+            break;
+
+    }
+}
+*/
+
+
+/*{
+    //while
+    //do while
+    //for
+
+    let i = 1;
+    while(i <11){
+
+        //code
+        console.log(i);
+        i++;
+
+
+    }
+}
+*/
+
+{
+     let i = 5;
+
+    do{
+        console.log(i);
+        i++
+
+    }
+    while(i < 10);
+}
