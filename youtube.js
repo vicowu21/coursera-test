57b7dbc91663285f78344ea0db81dcfe4badc4da
vicowu21 <85970139+vicowu21@users.noreply.github.com>
2022-07-17 22:18:25 +0000

    Create youtube.js

---
+++ youtube.js
@@ -0,0 +1,13 @@
+//var username = prompt(); //to take input from the user
+
+//alert(username);
+
+//console.log(username);
+
+ var age = 50;
+/*   console.log(age);
+   var newage = age;
+   console.log(newage);                               */
+
+var message = "Vicky = " + age;
+console.log(message);
